169368fbe056c9fcbf3e465a0725c30165c6590c
Thakur <ithakur@deloitte.com>
2026-01-05 12:43:20 +0530

    initila commit

---
+++ src/test/java/com/ai/service/demo/AiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.service.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
